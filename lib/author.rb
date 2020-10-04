248502907a2be454cf6405ba63cc99b89fa40237
Kristian Joos <kjoos22@gmail.com>
2020-10-04 19:25:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,8 +16,8 @@ class Author
   end
   
   def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    post.author = self
+    new_post = Post.new(post_title)
+    new_post.author = self
   end
   
 end
